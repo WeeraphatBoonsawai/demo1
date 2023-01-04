e3830fc669b09f1c5adbd015c273b8ff17aa9946
WeeraphatBoonsawai <117970214+WeeraphatBoonsawai@users.noreply.github.com>
2023-01-04 21:53:16 +0700

    Add files via upload

---
+++ proj/home.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/NAV.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Light novel</title>
+</head>
+<body>
+    <!-- nav -->
+<header class="header-area">
+        <div class="header-container">
+            <div class="site-logo">
+                <a href="home.php">Light <span>novel</span></a>
+            </div>
+            <div class="mobile-nav">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="site-nav-menu">
+                <ul class="primary-menu">
+                    <li><a href="home.php" class="active">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="book.php">book</a></li>
+                    <li><a href="shop.php">Shop</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section id="hero-section"></section>
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+    <script>
+        $(document).ready(function(){
+    $(".mobile-nav i").click(function(){
+        $(".site-nav-menu").toggleClass("mobile-menu");
+    });
+ });
+    </script>
+</div>
+<!-- nav -->
+
+<!-- หัว -->
+<div class="genshin">
+<div class="row">
+        <div class="imgWrapper">
+            <img src="img/home1-1.jpg" alt="">
+        </div>
+        <div class="contentWrapper">
+            <div class="content">
+                <span class="textWrapper">
+                    <span></span>ไลต์โนเวล
+                </span>
+                <h2>Light novel</h2>
+                <p>ไลต์โนเวล (ญี่ปุ่น: ライトノベル; โรมาจิ: raito noberu) ย่อว่า LN หมายถึงนิยายประเภทหนึ่งจากประเทศญี่ปุ่น เป็นนิยายสำหรับวัยรุ่นโดยเฉพาะกลุ่มนักเรียนมัธยมปลาย
+                     ไลต์โนเวลหนึ่งเล่มมีความยาว 50,000 คำ ซึ่งถือว่าใกล้เคียงกับเกณฑ์ขั้นต่ำของนวนิยายตะวันตก ไลต์โนเวลนิยมตีพิมพ์ด้วยรูปเล่มขนาด A5<br>
+                     ในประเทศญี่ปุ่น ไลต์โนเวล แบบ "ต้นตำรับ" นั้นได้มีการนำการใช้ฟุริงะนะเข้ามาใช้เป็นอย่างมากด้วยสองเหตุผล <br>
+                     หนึ่งคือสำหรับนักอ่านวัยเยาว์ที่ยังไม่ชำนาญในตัวอักษรคันจิให้สามารถเข้าใจในเนื้อหาได้ สองคือในหลายๆ เรื่องของ ไลต์โนเวล ผู้เขียนมักจะสร้างคำอ่านฟุริงะนะขึ้นมาใหม่ 
+                     ซึ่งไม่สามารถพบเห็นได้ในการใช้ภาษาญี่ปุ่นทั่วๆ ไป</p>
+                <a href="about.php">อ่านต่อ</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+<!-- หัว -->
+
+<!-- หัว1 -->
+<h1 class="mb-8 text-center text-3xl text-white mt-10 rounded-lg border-2 border-slate-100 bg-black ml-40 mr-40 shadow-2xl">มาแรงช่วงนี้</h1>
+<div class="genshin">
+<div class="row">
+        <div class="imgWrapper">
+            <img src="img/home1-2.jpg" alt="">
+        </div>
+        <div class="contentWrapper">
+            <div class="content">
+                <span class="textWrapper">
+                    <span></span>Otonari no Tenshi-sama ni Itsu no Ma ni ka Dame Ningen ni Sareteita Ken
+                </span>
+                <h2>ขาดคุณนางฟ้าข้างห้องไป ผมคงมีชีวิตต่อไปไม่ได้อีกแล้ว</h2>
+                <p>ชื่อภาษาอังกฤษ : The Angel Next Door Spoils Me Rotten <br>
+                เขียนโดย : ซาเอกิซัง<br>
+                วาดภาพประกอบโดย : ฮาเนะโคโตะ<br>
+                ตีพิมพ์โดย : เอสบีครีเอทีฟ<br>
+                ผู้จัดจำหน่ายภาษาไทย : อนิแม็ก<br>
+                ในเครือ : GA Bunko<br>
+                วางจำหน่ายตั้งแต่ : 15 มิถุนายน พ.ศ. 2562 – ปัจจุบัน <br>
+                    เรื่องราวเกี่ยวกับอามาเนะอาศัยอยู่คนเดียวในแมนชั่น กับมาฮิรุสาวงามอันดับหนึ่งของโรงเรียนอาศัยอยู่ห้องข้าง ๆ 
+                    กันกับอามาเนะ พวกเขาทั้งสองไม่มีความสัมพันธ์อะไรกันเลย กระทั่งในวันที่ฝนตก 
+                    อามาเนะได้เห็นมาฮิรุนั่งคนเดียวอยู่บนชิงช้าในที่สวนสาธารณะท่ามกลางสายฝน 
+                    โดยเธอไม่มีร่ม อามาเนะจึงยื่นร่มให้กับเธอ หลังจากนั้นความสัมพันธ์ที่ไม่มีชื่อเรียกก็ได้เริ่มต้นขึ้น</p>
+                <a href="about.php">อ่านต่อ</a>
+            </div>
+        </div>
+    </div>
+</div>
+<div>
+<!-- หัว1 -->
+
+<!-- หัว2 -->
+<div class="genshin" ></div>
+<h1 class="mb-8 text-center text-3xl text-white mt-10 rounded-lg border-2 border-slate-100 bg-black ml-40 mr-40 shadow-2xl">ถูกดัดแปลงเป็นอนิเมะ</h1>
+</div>
+<div class="ml-28 mr-28">
+<iframe class="w-full aspect-video" width="1280" height="720" src="https://www.youtube.com/embed/T8GjErJvtlM"></iframe>
+</div>
+<!-- หัว2 -->
+
+<!-- หัว3 -->
+<section class="md:h-full flex items-center text-gray-600">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="text-center mb-12">
+                <h5 class="text-base md:text-lg text-sky-600 mb-1">แนะนำ</h5>
+                <h1 class="text-4xl md:text-6xl text-gray-700 font-semibold">Light novel</h1>
+            </div>
+            <div class="flex flex-wrap -m-4">
+                <div class="p-4 sm:w-1/2 lg:w-1/3">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="https://images.alphacoders.com/106/thumbbig-1065299.webp" alt="blog">
+                        <div class="p-6 hover:bg-zinc-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-sky-600 mb-1">ขอต้อนรับสู่ห้องเรียนนิยม(เฉพาะ)ยอดคน</h2>
+                            <h1 class="text-2xl font-semibold mb-3">Classroom of the Elite</h1>
+                            <p class="leading-relaxed mb-3">ว่าด้วยการที่โรงเรียนดังที่ใครก็อยากเข้าเพราะถ้าสามารถเข้าโรงเรียนนี้
+                                ได้จะสามารถเข้ามหาลัยหรือทำงานที่ตนเองชอบไปอย่างอิสระแต่ในความเป็นจริงการอยู่ในรอดให้
+                                สังคมโรงเรียนนี้นมีความโหดร้ายมากกว่าที่ทุกคนเห็นทั้งการแข่งขันในระบบทีมที่คะแนนสอบ</p>
+                            <div class="flex items-center flex-wrap ">
+                                <a class="text-sky-600 inline-flex items-center md:mb-2 lg:mb-0">Read More
+                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-4 sm:w-1/2 lg:w-1/3">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="https://wallpaperaccess.com/full/4147115.jpg" alt="blog">
+                        <div class="p-6 hover:bg-zinc-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-sky-600 mb-1">โอเวอร์ลอร์ด</h2>
+                            <h1 class="text-2xl font-semibold mb-3">OVERLORD</h1>
+                            <p class="leading-relaxed mb-3">เป็นเรื่องราวของชายหนุ่มวัยทำคนหนึ่งที่เข้า 
+                                ไปเล่นเกมเพื่อละลึกความหลังเป็นครั้งสุดท้าย เพราะเชิฟเวอร์เกมกำลังจะปิดตัวลง แต่พอวินาทีสุดท้าย 
+                                ที่เชิฟกำลังปิดตัวพระเอกดันไม่กด Logour ออกจะเกม จึงทำให้พระเองที่อยู่ในร่างตัวละครและสุสานนาซาริกที่พระเองปกครองอยู่</p>
+                            <div class="flex items-center flex-wrap ">
+                                <a class="text-sky-600 inline-flex items-center md:mb-2 lg:mb-0">Read More
+                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="p-4 sm:w-1/2 lg:w-1/3">
+                    <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+                            src="https://images2.alphacoders.com/121/1219166.jpg" alt="blog">
+                        <div class="p-6 hover:bg-zinc-700 hover:text-white transition duration-300 ease-in">
+                            <h2 class="text-base font-medium text-sky-600 mb-1">โกนหนวดไปทำงานแล้วกลับบ้านมาพบเธอ</h2>
+                            <h1 class="text-2xl font-semibold mb-3">Hige wo Soru</h1>
+                            <p class="leading-relaxed mb-3">‘โยชิดะ’ หนุ่มออฟฟิศมนุษย์เงินเดือนธรรมดา 
+                                ที่พึ่งโดนสาวที่แอบชอบมานานกว่า 5 ปี อย่าง ‘โกโต้ ไอริ’ หักอก ทำให้เขาเสียใจมากจึงไปดื่มระบายกับเพื่อนร่วมงานอย่าง 
+                                ‘ฮาชิโมโตะ’ ทั้งสองดื่มจนดึกดื่นและแยกย้าย แต่ระหว่างทางกลับบ้านนั้น โยชิดะได้พบกับ ‘โองิวาระ ซายุ’ </p>
+                            <div class="flex items-center flex-wrap ">
+                                <a class="text-sky-600 inline-flex items-center md:mb-2 lg:mb-0">Read More
+                                    <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
+                                        fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<!-- หัว3 -->
+
+<!-- footer -->
+<div class="footer-basic">
+        <footer>
+            <div class="social"><a href="https://www.instagram.com/bakaimitai/?hl=th"><i class="icon ion-social-instagram"></i></a><a href="https://twitter.com/wee336677"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com/pan.boonsawai.5"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="index.php">Home</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Book</a></li>
+                <li class="list-inline-item"><a href="#">Shop</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Light novel © 2023</p>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+<!-- footer -->
+</body>
+</html>
---
+++ proj/shop.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="css/NAV.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>shop-Light novel</title>
+</head>
+<body>
+            <!-- nav -->
+<header class="header-area">
+        <div class="header-container">
+            <div class="site-logo">
+                <a href="home.php">Light <span>novel</span></a>
+            </div>
+            <div class="mobile-nav">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="site-nav-menu">
+                <ul class="primary-menu">
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="book.php">book</a></li>
+                    <li><a href="shop.php" class="active">Shop</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section id="hero-section3"></section>
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="app.js"></script>
+    <script>
+        $(document).ready(function(){
+    $(".mobile-nav i").click(function(){
+        $(".site-nav-menu").toggleClass("mobile-menu");
+    });
+ });
+    </script>
+</div>
+<!-- nav -->
+
+<!-- footer -->
+<div class="footer-basic">
+        <footer>
+            <div class="social"><a href="https://www.instagram.com/bakaimitai/?hl=th"><i class="icon ion-social-instagram"></i></a><a href="https://twitter.com/wee336677"><i class="icon ion-social-twitter"></i></a><a href="https://www.facebook.com/pan.boonsawai.5"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="index.php">Home</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Book</a></li>
+                <li class="list-inline-item"><a href="#">Shop</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">Light novel © 2023</p>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+<!-- footer -->
+</body>
+</html>
